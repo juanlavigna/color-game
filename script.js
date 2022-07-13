eef8aa9ad14b622e941ce59474e67435e95ecca5
Juan Lavigna <juanlavigna@gmail.com>
2022-07-13 14:22:07 -0300

    Refactor code

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const easy = document.querySelector("#easy")
+const hard = document.querySelector("#hard")
+const divs = document.querySelectorAll(".container>div");
+const colorDisplay = document.querySelector("#colorDisplay");
+const mensaje = document.querySelector("#message");
+const H1 = document.querySelector("h1");
+const nuevosColores = document.querySelector("#stripe>button");
+let colors = randomColors(+hard.value);
+let pickedColor = pickColor();
+
+game()
+
 function randomRGB (){
     let rand1 = Math.floor(Math.random()*255)
     let rand2 = Math.floor(Math.random()*255)
@@ -12,15 +24,6 @@ function randomColors(dificultad){
     }
     return colors
 }
-let easy = document.querySelector("#easy")
-let hard = document.querySelector("#hard")
-let colors = randomColors(+hard.value);
-let divs = document.querySelectorAll(".container>div");
-let colorDisplay = document.querySelector("#colorDisplay");
-let pickedColor = pickColor();
-let mensaje = document.querySelector("#message");
-let H1 = document.querySelector("h1");
-let nuevosColores = document.querySelector("#stripe>button");
 
 function changeColor (colorGanador) {
     divs.forEach((div) => {
@@ -33,23 +36,41 @@ function pickColor (){
     return colors[randomNumber]
 }
 
-for(let i = 0; i < divs.length; i++){
-    divs[i].style.backgroundColor = colors[i];
-    let clickedColor = divs[i].style.backgroundColor
-    let tempDiv = divs[i]
-    divs[i].addEventListener("click", (e) =>{
-        if(clickedColor == pickedColor){
-            mensaje.textContent = "Correcto!";
-            H1.style.color = pickedColor;
-            changeColor(pickedColor);
-            nuevosColores.textContent = "Jugar de nuevo!"
-        }else{
-            tempDiv.style.backgroundColor = "#232323";
-            mensaje.textContent = "Volvé a intentarlo";
-        }
-    })
-};
+easy.addEventListener("click", function() {
+    easy.classList.add("selected")
+    hard.classList.remove("selected")
+    game()
+    for(let i = 3; i < divs.length; i++){
+        divs[i].classList.add("hidden")
+    }
+})
 
+hard.addEventListener("click", function() {
+    hard.classList.add("selected")
+    easy.classList.remove("selected")
+    game()
+    for(let i = 3; i < divs.length; i++){
+        divs[i].classList.remove("hidden")
+    }
+})
+function game(){
+    for(let i = 0; i < divs.length; i++){
+        divs[i].style.backgroundColor = colors[i];
+        let clickedColor = divs[i].style.backgroundColor
+        let tempDiv = divs[i]
+        divs[i].addEventListener("click", (e) =>{
+            if(clickedColor == pickedColor){
+                mensaje.textContent = "Correcto!";
+                H1.style.color = pickedColor;
+                changeColor(pickedColor);
+                nuevosColores.textContent = "Jugar de nuevo!"
+            }else{
+                tempDiv.style.backgroundColor = "#232323";
+                mensaje.textContent = "Volvé a intentarlo";
+            }
+        })
+    };
+}
 colorDisplay.textContent = pickedColor
 
 
@@ -57,6 +78,3 @@ nuevosColores.addEventListener("click", function (){
     document.location.reload()
 })
 
-easy.addEventListener("click", function() {
-    
-})
